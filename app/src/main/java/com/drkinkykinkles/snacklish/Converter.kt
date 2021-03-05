6c053fcde8c21b0105eae2cf2f7c45f26f9e9a6d
Jacob Sharp <official.jake.sharp@gmail.com>
2021-03-05 02:32:51 -0700

    SNA-NA Feature: Mainnuttain originalamel wordelectable caseat

--- app/src/main/java/com/drkinkykinkles/snacklish/Converter.kt
+++ app/src/main/java/com/drkinkykinkles/snacklish/Converter.kt
@@ -3,9 +3,6 @@ package com.drkinkykinkles.snacklish
 class Converter {
     companion object {
         fun convert(text: String): String {
-            // lowercase all the text to avoid weird mixed-casing results
-            val loweredText = text.toLowerCase();
-
             // Load in the lists we will use to manipulate the text
             val blacklist = GetBlacklist();
             val fixedList = GetFixedList();
@@ -13,9 +10,12 @@ class Converter {
 
             // Grab all individual words in the text and convert them
             val wordRegex = "[\\w']+".toRegex();
-            return loweredText.replace(wordRegex) { result -> convertToken(result.value, blacklist, fixedList, ruleList) }
+            return text.replace(wordRegex) { result -> preserveCasing(result.value, convertToken(result.value.toLowerCase(), blacklist, fixedList, ruleList)) }
         }
 
+        /**
+         * Converts individual words into snacklish
+         */
         fun convertToken (token: String, blacklist: Set<String>, fixedList: Map<String, String>, ruleList: Map<String, String>): String {
             // Some simple words we do not want to replace
             if (blacklist.contains(token)) {
@@ -28,6 +28,7 @@ class Converter {
             }
 
             // Some words we can modify with special regulexes
+            // Key order is randomized so we can get some waaacky results
             for (rule in ruleList.map { it.key to it.value }.shuffled().toMap()) {
                 if (rule.key.toRegex().containsMatchIn(token)) {
                     return rule.key.toRegex().replace(token, rule.value);
@@ -36,6 +37,25 @@ class Converter {
 
             // If the word cannot be converted just return it
             return token;
+        }
+
+        /**
+         * Applies the original casing of a string to its new snacklish version
+         */
+        fun preserveCasing(original: String, result: String): String {
+            if (original.toLowerCase() == original) {
+                return result;
+            }
+
+            if (original.toUpperCase() == original) {
+                return result.toUpperCase();
+            }
+
+            if (original.capitalize() == original) {
+                return result.capitalize();
+            }
+
+            return result;
         }
     }
 }
