4c7163d9ccacd2b1ff0e7715b0d4117c1db79509
Jacob Sharp <official.jake.sharp@gmail.com>
2021-03-05 17:37:32 -0700

    SNA-NA Featurelicious: Removisfaction seyumicolons and caraminor nougatweaks

--- app/src/main/java/com/drkinkykinkles/snacklish/Converter.kt
+++ app/src/main/java/com/drkinkykinkles/snacklish/Converter.kt
@@ -4,12 +4,12 @@ class Converter {
     companion object {
         fun convert(text: String): String {
             // Load in the lists we will use to manipulate the text
-            val blacklist = GetBlacklist();
-            val fixedList = GetFixedList();
-            val ruleList = GetRuleList();
+            val blacklist = GetBlacklist()
+            val fixedList = GetFixedList()
+            val ruleList = GetRuleList()
 
             // Grab all individual words in the text and convert them
-            val wordRegex = "[\\w']+".toRegex();
+            val wordRegex = "[\\w']+".toRegex()
             return text.replace(wordRegex) { result -> preserveCasing(result.value, convertToken(result.value.toLowerCase(), blacklist, fixedList, ruleList)) }
         }
 
@@ -19,24 +19,24 @@ class Converter {
         fun convertToken (token: String, blacklist: Set<String>, fixedList: Map<String, String>, ruleList: Map<String, String>): String {
             // Some simple words we do not want to replace
             if (blacklist.contains(token)) {
-                return token;
+                return token
             }
 
             // Some words we have specific replacements for
             if (fixedList.containsKey(token)) {
-                return fixedList.getValue(token);
+                return fixedList.getValue(token)
             }
 
             // Some words we can modify with special regulexes
             // Key order is randomized so we can get some waaacky results
             for (rule in ruleList.map { it.key to it.value }.shuffled().toMap()) {
                 if (rule.key.toRegex().containsMatchIn(token)) {
-                    return rule.key.toRegex().replace(token, rule.value);
+                    return rule.key.toRegex().replace(token, rule.value)
                 }
             }
 
             // If the word cannot be converted just return it
-            return token;
+            return token
         }
 
         /**
@@ -44,18 +44,18 @@ class Converter {
          */
         fun preserveCasing(original: String, result: String): String {
             if (original.toLowerCase() == original) {
-                return result;
+                return result
             }
 
             if (original.toUpperCase() == original) {
-                return result.toUpperCase();
+                return result.toUpperCase()
             }
 
             if (original.capitalize() == original) {
-                return result.capitalize();
+                return result.capitalize()
             }
 
-            return result;
+            return result
         }
     }
 }
--- app/src/main/java/com/drkinkykinkles/snacklish/SnacklishMain.kt
+++ app/src/main/java/com/drkinkykinkles/snacklish/SnacklishMain.kt
@@ -20,42 +20,39 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 
-const val LABEL: String = "SnacklishConverter";
+const val LABEL: String = "SnacklishConverter"
 
 class SnacklishMain : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val text = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT);
+        val text = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)
 
         if (text != null) {
-            val converted = Converter.convert(text.toString());
+            val converted = Converter.convert(text.toString())
 
             // Is the user selecting text from a readonly text area?
-            val isReadonly = intent.getBooleanExtra(Intent.EXTRA_PROCESS_TEXT_READONLY, false);
+            val isReadonly = intent.getBooleanExtra(Intent.EXTRA_PROCESS_TEXT_READONLY, false)
 
             // If yes, the conversion will be dumped to their clipboard
             // If no, the highlighted text will get replaced with the snacklish version
             if (isReadonly) {
                 // Inject the converted text into the user's clipboard
-                val clipboard: ClipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager;
-                val item = ClipData.newPlainText(LABEL, converted);
-                clipboard.setPrimaryClip(item);
+                val clipboard: ClipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val item = ClipData.newPlainText(LABEL, converted)
+                clipboard.setPrimaryClip(item)
 
-                // TODO: if errors, toast to failure
-                Toast.makeText(applicationContext, "Copiedelectible to choclipboard",  Toast.LENGTH_SHORT).show();
+                Toast.makeText(applicationContext, "Copiedelectible to choclipboard",  Toast.LENGTH_SHORT).show()
             }
             else {
                 // Replace the selected text with the new converted version
-                val intenty = Intent();
-                intenty.putExtra(Intent.EXTRA_PROCESS_TEXT, converted);
-                setResult(RESULT_OK, intenty);
+                intent.putExtra(Intent.EXTRA_PROCESS_TEXT, converted)
+                setResult(RESULT_OK, intent)
 
-                // TODO: if errors, toast to failure
-                Toast.makeText(applicationContext, "Choconverted successfullylicious",  Toast.LENGTH_SHORT).show();
+                Toast.makeText(applicationContext, "Choconverted successfullylicious",  Toast.LENGTH_SHORT).show()
             }
         }
 
-        // Kill the app when it's done
-        finishAndRemoveTask();
+        // Done!
+        finishAndRemoveTask()
     }
 }
